c58d080c459883215a49d731f1f05b8da5ecd068
Riley Flynn <riley@rileyflynn.me>
2026-04-06 12:05:43 +0200

    borik-meta: bump version number

--- Formula/borik-meta.rb
+++ Formula/borik-meta.rb
@@ -2,7 +2,7 @@ class BorikMeta < Formula
   desc "Meta package containing dependencies for compiling and running Borik"
   homepage "https://github.com/fogo-sh/borik"
   url "file:///dev/null"
-  version "1.0.0"
+  version "1.0.1"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "imagemagick-full"
